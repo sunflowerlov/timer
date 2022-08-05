f6c3a90b4c868c53d97b915f51eb26995c5c5fa3
Lisha Liang <lishaliang1999@gmail.com>
2022-08-05 19:54:30 +0000

    beep

--- timer1.js
+++ timer1.js
@@ -1,15 +1,20 @@
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
+//console.log(args)
 
-const time = function (delay) {
-  delay = Number(delay);
-  if (!isNaN(delay)) {
-    setTimeout(beep, delay * 1000)
+const time = function (args) {
+  args.sort(function (a, b) {
+    return a - b;
+  })
+  console.log(args)
+  for (const each of args) {
+    if (!isNaN(Number(each))) {
+      if (each > 0) {
+        setTimeout(() => {
+          process.stdout.write('\x07')
+        }, each * 1000)
+      }
+    }
   }
-  
 }
-function beep() {
-  process.stdout.write('\x07');
-}
-for (const num of args) {
-  time(num)
-}
+
+time(args)
